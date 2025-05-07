a49c4ddadec0354f0f8faecb2e1e8b04b6337db0
Prakhar <prakharojha12@gmail.com>
2025-05-07 22:46:26 +0530

    Polished and final state

--- escrow-agent/utils/deploy.js
+++ escrow-agent/utils/deploy.js
@@ -1,93 +1,106 @@
-import fs from 'fs';
-import * as dotenv from 'dotenv';
-dotenv.config({ path: './.env.development.local' });
-import { parseSeedPhrase } from 'near-seed-phrase';
-import * as nearAPI from 'near-api-js';
+import fs from "fs";
+import * as dotenv from "dotenv";
+dotenv.config({ path: "./.env.development.local" });
+import { parseSeedPhrase } from "near-seed-phrase";
+import * as nearAPI from "near-api-js";
 const { Near, Account, KeyPair, keyStores } = nearAPI;
 
-const networkId = 'mainnet';
+const networkId = "testnet";
 const accountId = process.env.NEAR_ACCOUNT_ID;
 const contractId = process.env.NEAR_CONTRACT_ID;
 
 const { secretKey } = parseSeedPhrase(process.env.NEAR_SEED_PHRASE);
+
 const keyStore = new keyStores.InMemoryKeyStore();
 const keyPair = KeyPair.fromString(secretKey);
+
 keyStore.setKey(networkId, accountId, keyPair);
 keyStore.setKey(networkId, contractId, keyPair);
 
 const config = {
-    networkId,
-    keyStore,
-    nodeUrl: 'https://rpc.near.org',
-    walletUrl: 'https://mynearwallet.com/',
-    explorerUrl: 'https://nearblocks.io',
+  networkId,
+  keyStore,
+  nodeUrl:
+    networkId == "testnet"
+      ? "https://test.rpc.fastnear.com"
+      : "https://rpc.near.org",
+  walletUrl:
+    networkId == "testnet"
+      ? "https://testnet.mynearwallet.com/"
+      : "https://mynearwallet.com/",
+  explorerUrl:
+    networkId == "testnet"
+      ? "https://testnet.nearblocks.io"
+      : "https://nearblocks.io",
 };
 const near = new Near(config);
 const { connection } = near;
-const gas = BigInt('300000000000000');
+const gas = BigInt("300000000000000");
 
 export const getAccount = (id) => new Account(connection, id);
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
 const deploy = async () => {
-    try {
-        const account = getAccount(contractId);
-        await account.deleteAccount(accountId);
-    } catch (e) {
-        console.log('error deleteAccount', e);
-    }
+  try {
+    const account = getAccount(contractId);
+    await account.deleteAccount(accountId);
+  } catch (e) {
+    console.log("error deleteAccount", e);
+  }
 
-    await sleep(1000);
+  await sleep(1000);
 
-    try {
-        const account = getAccount(accountId);
-        await account.createAccount(
-            contractId,
-            keyPair.getPublicKey(),
-            nearAPI.utils.format.parseNearAmount('10'),
-        );
-    } catch (e) {
-        console.log('error createAccount', e);
-    }
+  try {
+    const account = getAccount(accountId);
+    console.log(keyPair);
 
-    await sleep(1000);
+    await account.createAccount(
+      contractId,
+      keyPair.getPublicKey(),
+      nearAPI.utils.format.parseNearAmount("10")
+    );
+  } catch (e) {
+    console.log("error createAccount", e);
+  }
 
-    const file = fs.readFileSync('./contract/target/near/contract.wasm');
-    let account = getAccount(contractId);
-    await account.deployContract(file);
-    console.log('deployed bytes', file.byteLength);
-    const balance = await account.getAccountBalance();
-    console.log('contract balance', balance);
+  await sleep(1000);
 
-    await sleep(1000);
+  const file = fs.readFileSync("./contract/target/near/contract.wasm");
+  let account = getAccount(contractId);
+  await account.deployContract(file);
+  console.log("deployed bytes", file.byteLength);
+  const balance = await account.getAccountBalance();
+  console.log("contract balance", balance);
 
-    const initRes = await account.functionCall({
-        contractId,
-        methodName: 'init',
-        args: {
-            owner_id: accountId,
-        },
-        gas,
-    });
+  await sleep(1000);
 
-    console.log('initRes', initRes);
+  const initRes = await account.functionCall({
+    contractId,
+    methodName: "init",
+    args: {
+      owner_id: accountId,
+    },
+    gas,
+  });
 
-    await sleep(1000);
+  console.log("initRes", initRes);
 
-    // NEEDS TO MATCH docker-compose.yaml CODEHASH
-    const codehash =
-        '61923f6f77b607c96420d253e4eaa0764a125728cbe501a95c4477cb2a5e2351';
-    account = getAccount(accountId);
-    const approveRes = await account.functionCall({
-        contractId,
-        methodName: 'approve_codehash',
-        args: {
-            codehash,
-        },
-        gas,
-    });
+  await sleep(1000);
 
-    console.log('approveRes', approveRes);
+  // NEEDS TO MATCH docker-compose.yaml CODEHASH
+  const codehash =
+    "0cecbd3135be0ae01142fefe16f2176c8c54ba29440fa9dfe69ff6bcb8777592";
+  account = getAccount(accountId);
+  const approveRes = await account.functionCall({
+    contractId,
+    methodName: "approve_codehash",
+    args: {
+      codehash,
+    },
+    gas,
+  });
+
+  console.log("approveRes", approveRes);
 };
 
 deploy();
