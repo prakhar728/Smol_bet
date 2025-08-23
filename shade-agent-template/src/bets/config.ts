79716cb2a8556751fafd79eabe1ab4b61a4e9477
Prakhar Ojha <prakharojha@Prakhars-MacBook-Pro.local>
2025-08-23 17:33:01 +0530

    Added constants in config.ts

---
+++ shade-agent-template/src/bets/config.ts
@@ -0,0 +1,13 @@
+
+// BOT CONFIGURATIONS
+export const BOT_NAME = process.env.BOT_NAME || "smol_bet";
+export const BOT_ID = process.env.BOT_ID || "1869279598522896384";
+
+
+// LOOP CONSTANTS
+export const REPLY_PROCESSING_DELAY = 30000;
+export const DEPOSIT_PROCESSING_DELAY = 60000;
+export const SETTLEMENT_PROCESSING_DELAY = 30000;
+export const MAX_DEPOSIT_ATTEMPTS = 12 * 60; // 12 per minute * 60 mins
+export const POLLING_INTERVAL = 5 * 60 * 1000; // 5 minutes between search polls
+
